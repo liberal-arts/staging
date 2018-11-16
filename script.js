2c7e3d744f22033b66ec77b51d8010e207ecf33b
Hemlocked <phantomofrotten@gmail.com>
2018-11-16 21:56:50 +0900

    FirstCommit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+(function($){
+    $(function(){
+ $('#loopSlide ul').simplyScroll({
+            autoMode: 'loop',
+            speed: 1,
+            frameRate: 24,
+            horizontal: true,
+            pauseOnHover: false,
+            pauseOnTouch: false
+        });
+          
+    });
+})(jQuery);
